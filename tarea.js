f9b3f8eca2555464eb882c7e0b1190175deb27e6
Susana Medel <susana.medel@genstudents.org>
2024-05-07 12:22:05 -0400

    Entrega

--- tarea.js
+++ tarea.js
@@ -2,9 +2,9 @@
 
 // Debe solicitar al usuario 3 números por prompt 
 // y guardarlos en sus respectivas variables.
-// let num1 = parseInt(prompt("Ingrese un numero: "))
-// let num2 = parseInt(prompt("Ingrese un numero: "))
-// let num3 = parseInt(prompt("Ingrese un numero: "))
+let num1 = parseInt(prompt("Ingrese un numero: "))
+let num2 = parseInt(prompt("Ingrese un numero: "))
+let num3 = parseInt(prompt("Ingrese un numero: "))
 
 //Debe analizar los números, identificar cual es el número mayor,
 //  el número del centro y el número menor
@@ -44,8 +44,12 @@ menor = num2}
 }
 }
 
-comparar(4,4,2)
-comparar(4,2,4)
-comparar(2,4,4)
-comparar(3,3,3)
-comparar(3,1,9)
+// test, salen todos bien :3
+// comparar(4,4,2)
+// comparar(4,2,4)
+// comparar(2,4,4)
+// comparar(3,3,3)
+// comparar(3,1,9)
+
+// test con prompt html
+comparar(num1,num2,num3)
